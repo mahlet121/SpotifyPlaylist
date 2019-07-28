93f8b56e2b5e00c50f9084a67c11c456df4e11b0
unknown <mahleta333@gmail.com>
2019-07-27 19:56:52 -0500

    spotifyplaylist

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.get('/callback', function(req, res) {
   })
 })
 
-let port = process.env.PORT || 8888
+let port = process.env.PORT || 3000
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
 app.listen(port)
